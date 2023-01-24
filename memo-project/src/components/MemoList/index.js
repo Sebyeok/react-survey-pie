a1bfb7b38a4162bb7169b436fc89fe0af854bf4b
Sebyeok <tngnu02@gmail.com>
2023-01-24 20:02:32 +0900

    update

---
+++ memo-project/src/components/MemoList/index.js
@@ -0,0 +1,30 @@
+import MemoItem from '../MemoItem';
+import './index.css';
+function MemoList({
+  memos,
+  setSelectedMemoIndex,
+  selectedMemoIndex,
+  deleteMemo,
+}) {
+  return (
+    <div>
+      {memos.map((memo, index) => (
+        <MemoItem
+          key={index}
+          onClickItem={() => {
+            setSelectedMemoIndex(index);
+          }}
+          onClickDelete={(e) => {
+            deleteMemo(index);
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+          isSelected={index === selectedMemoIndex}
+        >
+          {memo.title}
+        </MemoItem>
+      ))}
+    </div>
+  );
+}
+export default MemoList;
